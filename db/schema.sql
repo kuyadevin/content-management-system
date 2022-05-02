343097e8112f20e216327e956b6d4e1aab97338b
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-01 21:41:16 -0700

    Added express, mysql12, inquirer, ,iddleware, and set port

--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY manager_id
+    FOREIGN KEY role_id
     REFERENCES role(id)
 )
---
+++ server.js
@@ -0,0 +1,13 @@
+// Import and require express
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+// Import and require Inquirer
+const inquirer = require('inquirer');
+// Set PORT
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
