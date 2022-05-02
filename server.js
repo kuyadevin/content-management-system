4e56a9baedf983e571897cfae62a38573bf7d5b3
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-01 22:01:02 -0700

    Added connection to mysql

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the company_db database.`)
+);
