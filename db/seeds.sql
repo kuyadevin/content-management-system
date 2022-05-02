fcb4ecc23027f598ea57186afe902e12cafdc66a
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-01 22:46:04 -0700

    Added seed data for role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,13 @@ VALUES (1, "Sales"),
        (3, "Engineering"),
        (4, "Human Resources"),
        (5, "Legal");
+
+INSERT INTO role (id, title, salary)
+VALUES (1, "Sales Lead", 100000),
+       (2, "Sales Person", 50000),
+       (3, "Account Manager", 150000),
+       (4, "Accountant", 100000),
+       (5, "Senior Engineer", 200000),
+       (6, "Junior Engineer", 150000),
+       (7, "HR Manager", 100000),
+       (8, "Lawyer", 200000);
