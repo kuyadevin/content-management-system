f6e255ac2d5eb11e6b038c107b4f45a9496f505f
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-01 22:28:38 -0700

    Added seed for department table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,6 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-
-USE company_db;
-
-CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
-);
-
-CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-);
-
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY manager_id
-    REFERENCES role(id)
-)
+INSERT INTO department (id, name)
+VALUES (1, "Sales"),
+       (2, "Finance"),
+       (3, "Engineering"),
+       (4, "Human Resources"),
+       (5, "Legal");
