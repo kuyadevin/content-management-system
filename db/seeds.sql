8c3f3f08b4b9d08f7ed3833fc6f71a316d8ca7a3
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-01 23:09:25 -0700

    Added seed to employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,12 +5,22 @@ VALUES (1, "Sales"),
        (4, "Human Resources"),
        (5, "Legal");
 
-INSERT INTO role (id, title, salary)
-VALUES (1, "Sales Lead", 100000),
-       (2, "Sales Person", 50000),
-       (3, "Account Manager", 150000),
-       (4, "Accountant", 100000),
-       (5, "Senior Engineer", 200000),
-       (6, "Junior Engineer", 150000),
-       (7, "HR Manager", 100000),
-       (8, "Lawyer", 200000);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 1),
+       (2, "Sales Person", 50000, 1),
+       (3, "Account Manager", 150000, 2),
+       (4, "Accountant", 100000, 2),
+       (5, "Senior Engineer", 200000, 3),
+       (6, "Junior Engineer", 150000, 3),
+       (7, "HR Manager", 100000, 4),
+       (8, "Lawyer", 200000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Chris", "Evans", 1, null),
+       (2, "Robert", "Downey", 2, 1),
+       (3, "Chris", "Hemsworth", 3, null),
+       (4, "Mark", "Ruffalo", 4, 3),
+       (5, "Scarlett", "Johansson", 5, null),
+       (6, "Jeremy", "Renner", 6, 5),
+       (7, "Chris", "Pratt", 7, null),
+       (8, "Zoe", "Saldana", 8, null);
